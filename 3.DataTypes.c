59bd3c4efbc49e019c5f503a1a51057be8493dfe
Tony Olsson <support.tony@gmail.com>
2019-04-03 23:22:55 +0200

    Add files via upload

---
+++ 3.DataTypes.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+    /*
+    int a=5, b=10;
+    int c;
+    c=a+b;
+    printf("Sum = %d",c);
+    printf("\nSum of %d and %d is %d",a,b,c);
+
+    char gender = 'm';
+    printf("\nGender is : %c", gender);
+
+    int age = 30;
+    char gender = 'm';
+    float weight = 67.5f;
+    printf("\nHi, I am %d old, gender : %c and weight : %f", age, gender, weight);
+    */
+
+    //char data = 5;
+    //printf("Data value is %d", data);
+    /*
+    char a = 5;
+    char b = 10;
+    char c = a + b;
+    printf("Sum = %d",c);
+    */
+    char data = 129;  // Max 127
+    //unsigned char data = 256; // Max 255
+    printf("%d", data);
+}
